5bf7dd97039ec0ef2bd89c5c43269882d20eee45
Sam <sjoseph1848@gmail.com>
2019-01-20 22:03:17 -0600

    Added a customer class to the DLL, with logic

---
+++ Airline.BL/Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Airline.BL
+{
+    public class Address
+    {
+        public Address()
+        {
+
+        }
+        public Address(int addressId)
+        {
+            this.AddressId = addressId;
+        }
+        public int AddressId { get; set; }
+        public int AddressType { get; set; }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
--- Airline.BL/Customer.cs
+++ Airline.BL/Customer.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Airline.BL
 {
     public class Customer
     {
+        public List<Address> AddressList { get; set; }
+
+        public Customer(int customerFlightId)
+        {
+            this.CustomerFlightId = customerFlightId;
+            AddressList = new List<Address>();
+        }
+
+        public int CustomerFlightId { get; set; }
     }
 }
